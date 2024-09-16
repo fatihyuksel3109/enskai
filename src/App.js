16397e070940756fcf4bb895f7d9ddcbbc089428
Fatih Yuksel <fatihyuksel3109@gmail.com>
2024-09-16 09:04:05 +0300

    comment removal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { NotificationProvider } from './contexts/NotificationContext';
